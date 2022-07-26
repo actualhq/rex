8ff178fe6041d53f43a4a1650fa0147460005ddb
Shawn Tabai <shawn.tabai@actualhq.com>
2022-07-25 23:27:54 -0700

    Do os-dependent test filtering in ignore test param

--- shell/executables_test.ts
+++ shell/executables_test.ts
@@ -2,11 +2,10 @@ import { assert, assertRejects } from '../testing_deps.ts';
 import { makeFileExecutable } from './executables.ts';
 import { $ } from './run.ts';
 
-Deno.test('new file becomes executable on unix/mac', async () => {
-  if (Deno.build.os === 'windows') {
-    return;
-  }
-
+Deno.test({
+  name: 'new file becomes executable on unix/mac',
+  ignore: Deno.build.os === 'windows',
+}, async () => {
   const filename = await Deno.makeTempFile();
   Deno.writeTextFile(filename, 'echo "looks good"');
   assertRejects(() => $([filename]), 'File execution succeeded before permission flag was set');
@@ -15,11 +14,10 @@ Deno.test('new file becomes executable on unix/mac', async () => {
   assert(await $([filename]), 'File execution failed after permission flag was set');
 });
 
-Deno.test('does not blow up on windows', async () => {
-  if (Deno.build.os !== 'windows') {
-    return;
-  }
-
+Deno.test({
+  name: 'does not blow up on windows',
+  ignore: Deno.build.os !== 'windows',
+}, async () => {
   const filename = await Deno.makeTempFile();
   await makeFileExecutable(filename);
 });
