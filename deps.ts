ee6a794ec1778684c8a12e6bb5642555931b9182
Shawn Tabai <shawn.tabai@actualhq.com>
2022-08-04 22:09:09 -0700

    Add checksum calculation functions

--- deps.ts
+++ deps.ts
@@ -1,4 +1,9 @@
-export { EOL } from 'https://deno.land/std@0.145.0/fs/eol.ts';
+export { crypto } from 'https://deno.land/std@0.150.0/crypto/mod.ts';
+export { EOL } from 'https://deno.land/std@0.150.0/fs/eol.ts';
+export { iterateReader } from 'https://deno.land/std@0.150.0/streams/conversion.ts';
+
+export * as hex from 'https://deno.land/std@0.150.0/encoding/hex.ts';
+export * as path from 'https://deno.land/std@0.150.0/path/mod.ts';
 
 export { default as isNil } from 'https://cdn.skypack.dev/lodash@v4.17.21/isNil?dts';
 export { default as isEmpty } from 'https://cdn.skypack.dev/lodash@v4.17.21/isEmpty?dts';
