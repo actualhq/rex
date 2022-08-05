ec6ca26fbe7c86cb8cf11136165edec310781f02
Shawn Tabai <shawn.tabai@actualhq.com>
2022-08-04 22:36:22 -0700

    Test without hex encoding bytes

--- crypto/checksum_test.ts
+++ crypto/checksum_test.ts
@@ -5,10 +5,10 @@ import { assertEquals } from '../testing_deps.ts';
 const TEST_TEXT_DATA = 'testing 1 2 3...';
 const TEST_TEXT_HASH = '2816d302127f4664a9f8de2f71bd42b7897897c80837c9bbce8a9dc60000182c';
 
-const TEST_BINARY_DATA = hex.decode(new TextEncoder().encode(
+const TEST_BINARY_DATA = new TextEncoder().encode(
   '403f3e3d3c3b3a393837363534333231302f2e2d2c2b2a292827262524232221201f1e1d1c1b1a19181716151413',
-));
-const TEST_BINARY_HASH = 'db8ea94d9fadffb5deaf217d25556f5c1c114b3fac46bb899ea02a6b398a4a38';
+);
+const TEST_BINARY_HASH = 'f9dacbc195283df782d7e5ec525b1d5492dffb2188abb6420a9302c4308538ac';
 
 Deno.test('correctly computes hash for text string', async () => {
   const hash = await checksumHex(TEST_TEXT_DATA);
