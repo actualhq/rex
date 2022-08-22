e4913d83fe7f30c5f3cfd617b859c9cd9ba2b8c6
Shawn Tabai <shawn.tabai@actualhq.com>
2022-08-22 09:14:44 -0700

    Add functionality to download files to file system

--- deps.ts
+++ deps.ts
@@ -1,7 +1,10 @@
 export { crypto } from 'https://deno.land/std@0.150.0/crypto/mod.ts';
 export { EOL } from 'https://deno.land/std@0.150.0/fs/eol.ts';
 export { iterateReader } from 'https://deno.land/std@0.150.0/streams/conversion.ts';
+export { parseMediaType } from 'https://deno.land/std@0.150.0/media_types/mod.ts';
 
+export * as base64 from 'https://deno.land/std@0.150.0/encoding/base64.ts';
+export * as fs from 'https://deno.land/std@0.150.0/fs/mod.ts';
 export * as hex from 'https://deno.land/std@0.150.0/encoding/hex.ts';
 export * as path from 'https://deno.land/std@0.150.0/path/mod.ts';
 
--- testing_deps.ts
+++ testing_deps.ts
@@ -2,6 +2,72 @@ export {
   assert,
   assertEquals,
   assertFalse,
+  assertInstanceOf,
   assertNotEquals,
   assertRejects,
-} from 'https://deno.land/std@0.145.0/testing/asserts.ts';
+  fail,
+} from 'https://deno.land/std@0.150.0/testing/asserts.ts';
+
+import { Server } from 'https://deno.land/std@0.150.0/http/server.ts';
+import { isNil } from './deps.ts';
+import { checkExhaustive } from './lang/types.ts';
+
+/** A simple HTTP server that can be manipulated for testing. */
+export interface TestServer {
+  urlOrigin: string;
+  close: () => Promise<void>;
+}
+
+/** Creates HTTP headers for serving a file download/attachment. */
+export function fileAttachmentHeaders(
+  fileEncoding: 'binary' | 'text',
+  filename?: string,
+): Record<string, string> {
+  const headers: Record<string, string> = {};
+  switch (fileEncoding) {
+    case 'binary':
+      headers['Content-Type'] = 'application/octet-stream';
+      break;
+    case 'text':
+      headers['Content-Type'] = 'text/plain';
+      break;
+    default:
+      checkExhaustive(fileEncoding);
+      break;
+  }
+  const attachmentSuffix = isNil(filename) ? '' : `; filename="${filename}"`;
+  headers['Content-Disposition'] = 'attachment' + attachmentSuffix;
+  return headers;
+}
+
+export function createStaticTestHttpServer(body: BodyInit, params: ResponseInit = {}): TestServer {
+  return createTestHttpServer(() => {
+    return new Response(body, { ...params, status: 200 });
+  });
+}
+
+/** Creates an HTTP server for testing that uses the supplied handler for all requests. */
+export function createTestHttpServer(handler: (req: Request) => Response): TestServer {
+  const testServerPort = 65432;
+  const server = new Server({
+    port: testServerPort,
+    handler,
+  });
+  const servingFinished = server.listenAndServe();
+  return {
+    urlOrigin: `http://0.0.0.0:${testServerPort}`,
+    close: () => {
+      server.close();
+      return servingFinished;
+    },
+  };
+}
+
+/** Creates a deterministic array of binary data with the specified number of bytes. */
+export function deterministicByteArray(size: number): Uint8Array {
+  const buffer: number[] = [];
+  for (let i = 0; i < size; i++) {
+    buffer.push((size - i) % 256);
+  }
+  return new Uint8Array(buffer);
+}
